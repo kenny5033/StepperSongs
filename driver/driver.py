81a8d0f5fe53aee68e32fd63c94e08d738353670
kenny5033 <kenneth.howes53@gmail.com>
2025-04-19 14:52:01 -0400

    serial port keep alive

--- driver/driver.py
+++ driver/driver.py
@@ -24,9 +24,6 @@ NOTE_FREQUENCIES: list[int] = {
     "B":  1012,
 }
 
-with serial.Serial(SERIAL_PORT, baudrate=9600, timeout=1) as ser:
-    ser.read() # clear any existing data in the buffer
-
 class Note:
     def __init__(self, note: str, duration: int):
         if note not in NOTE_FREQUENCIES:
@@ -34,18 +31,21 @@ class Note:
 
         self.frequency = NOTE_FREQUENCIES[note]
         self.duration = duration
+        self.ser = serial.Serial(SERIAL_PORT, baudrate=9600, timeout=1)
+
+    def __del__(self):
+        self.ser.close()
 
     def to_bytes(self) -> None:
         return struct.pack('>HH', self.frequency, self.duration)
 
-def send_note_via_serial(note, serial_port: str = SERIAL_PORT) -> None:
-    if not os.path.exists(serial_port):
-        raise FileNotFoundError(f"The serial port {serial_port} does not exist.")
+    def send_note_via_serial(self, serial_port: str = SERIAL_PORT) -> None:
+        if not os.path.exists(serial_port):
+            raise FileNotFoundError(f"The serial port {serial_port} does not exist.")
 
-    try:
-        with serial.Serial(serial_port, baudrate=9600, timeout=1) as ser:
-            if not ser.is_open:
+        try:
+            if not self.ser.is_open:
                 raise serial.SerialException("Failed to open serial port.")
-            ser.write(note.to_bytes())
-    except serial.SerialException as e:
-        print(f"Serial error: {e}")
+            self.ser.write(self.to_bytes())
+        except serial.SerialException as e:
+            print(f"Serial error: {e}")
